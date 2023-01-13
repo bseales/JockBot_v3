26db6ef2934a504820a8882f87ecdc2d4c08ff8a
Batson Seales <batson.seales@gmail.com>
2023-01-13 13:49:58 -0600

    posterity commit, working on ability to get user inputs

---
+++ src/commands/nfl/logo.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+import { JockbotCommand } from 'src/interfaces/command'
+import { Team } from 'src/interfaces/espn/nfl'
+
+export default class NFLLogo implements JockbotCommand {
+	public name = 'nfl-logo'
+	public description = 'Returns the logo of an NFL team.'
+
+	public commandBuilder = new SlashCommandBuilder()
+		.setName(this.name)
+		.setDescription(this.description)
+		.addStringOption(option =>
+			option
+				.setName('team')
+				.setRequired(true)
+				.setDescription('The team whose logo you would like.'))
+
+	public async execute(interaction: CommandInteraction): Promise<void> {
+		await interaction.reply(`${interaction.options.get('user')}`)
+		// const embed = await this.buildEmbed(interaction)
+        
+		// await interaction.reply({
+		// 	embeds: [embed]
+		// })
+	}
+
+	public async buildEmbed(interaction: CommandInteraction): Promise<EmbedBuilder> {
+		const team = await this.getTeam(interaction.options.getString('team'))
+		
+
+		return new EmbedBuilder({
+			title: team.displayName,
+			url: team.logos?.[0].href
+		})
+	}
+
+	public async getTeam(espnId: string): Promise<Team> {
+		const json = await axios.get(`http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/${espnId}`)
+		const team = json.data
+
+		return team
+	}
+    
+}
