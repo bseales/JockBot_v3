3286c9048f48cc94135b946454ade4e3ef4834d6
Batson Seales <batson.seales@gmail.com>
2023-01-21 17:01:06 -0600

    formatted commands file better

--- src/commands.ts
+++ src/commands.ts
@@ -3,7 +3,8 @@ import { SlashCommandBuilder } from 'discord.js'
 const JockbotCommands = [
 	new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Returns Pong!').toJSON(),
+		.setDescription('Returns Pong!')
+		.toJSON(),
 
 	new SlashCommandBuilder()
 		.setName('nfl')
@@ -23,31 +24,21 @@ const JockbotCommands = [
 						.setDescription('The team whose logo you would like.')))
 		.toJSON(),
 
-	// new SlashCommandBuilder()
-	// 	.setName('nfl-scores')
-	// 	.setDescription('Returns live NFL scores.').toJSON(),
-
 	new SlashCommandBuilder()
 		.setName('claim')
-		.setDescription('Claims a random amount of bux.').toJSON(),
-
-	// new SlashCommandBuilder()
-	// 	.setName('nfl-logo')
-	// 	.setDescription('Returns the logo of an NFL team.')
-	// 	.addStringOption(option =>
-	// 		option
-	// 			.setName('team')
-	// 			.setRequired(true)
-	// 			.setDescription('The team whose logo you would like.')).toJSON(),
+		.setDescription('Claims a random amount of bux.')
+		.toJSON(),
 
 	new SlashCommandBuilder()
 		.setName('set-nfl-odds')
 		.setDescription('Sets this week\'s NFL odds.')
-		.setDefaultMemberPermissions(0), // command disabled by default
+		.setDefaultMemberPermissions(0) // command disabled by default
+		.toJSON(),
 
 	new SlashCommandBuilder()
 		.setName('balance')
-		.setDescription('Returns your current balance.').toJSON(),
+		.setDescription('Returns your current balance.')
+		.toJSON(),
 
 	new SlashCommandBuilder()
 		.setName('bet')
